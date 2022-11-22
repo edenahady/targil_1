02eb3bf1e065ac4ee884e00135da73a4f06e12e5
edenahady <edenahadi@gmail.com>
2022-11-22 20:45:33 +0200

    fix

--- project/distances.cpp
+++ project/distances.cpp
@@ -7,88 +7,115 @@
 
 using namespace std;
 // >>>>>>> 5f32e8814c42ffe37ad185c6ce17885be41eb04e
-class distances{
 
-	distances::distances(vector<double> v1, vector<double> v2) {};
-	distances::distances(){};
-	void distances:: manhattan_distance(double dif_arr[])
+
+Distances::Distances(vector<double> v1, vector<double> v2) {}
+Distances::Distances(){};
+
+vector<double> Distances:: Difference_Calculator(vector<double>& v1, vector<double>& v2)
+{
+	double X_Dif, Y_Dif, Z_Dif;
+	vector<double> Dif_V;
+	for (int i=0; i<v1.size(); i++)
 	{
-		double distance = dif_arr[0] + dif_arr[1] + dif_arr[2];
-		cout << setprecision(10) << distance;
+		Dif_V[i] = abs(v1[i] - v2[i]);
 	}
-	double * distances:: difference_calculator(double arr1[], double arr2[])
-	{
-		double x_dif, y_dif, z_dif;
-		double dif_arr[3] = {};
-
-		dif_arr[0] = abs(arr1[0] - arr2[0]);
-		dif_arr[1] = abs(arr1[1] - arr2[1]);
-		dif_arr[2] = abs(arr1[2] - arr2[2]);
-		return dif_arr;
-	}
-
-
-	void distances::euclidean(double dif_arr[])
-	{
-		
-
-		double dist = pow(dif_arr[0], 2) + pow(dif_arr[1], 2) + pow(dif_arr[2], 2);       
-		dist = sqrt(dist);
-
-		cout << setprecision(10) << dist;
-	}
-
-
-	void distances:: chebyshev(double dif_arr[])
-	{
-		double max_temp = max(dif_arr[0], dif_arr[1]);
-		max_temp = max(max_temp, dif_arr[2]);
-		cout << setprecision(10) << max_temp;
-	}
-
-	void distances:: canberra(double dif_arr[], double arr1[], double arr2[])
-	{
-		double sol1 = dif_arr[0] / (arr1[0] + arr2[0]);
-		double sol2 = dif_arr[1] / (arr1[1] + arr2[1]);
-		double sol3 = dif_arr[2] / (arr1[2] + arr2[2]);
-		double dist = sol1 + sol2 + sol3;
-		cout << setprecision(10) << dist;
-	}
-
-	void distances:: minkowski(double dif_arr[])
-	{
-		int sum = 0;
-		for (int i = 0; i < 3; i++)
-		{
-			sum = sum + pow(dif_arr[i], 2);
-		}
-		cout<< setprecision(10) << sqrt(sum);
-	}
-
-	int main()
-	{
-		vector <double> v1{1, 2, 3};
-		vector <double> v2{3, 2, 1};
-		
-		// double arry1[3];
-		// for (int i = 0; i < 3; i++)
-		// {
-		// 	cin >> arry1[i];
-		// }
-		// cout<< endl;
-		// double arry2[3];
-
-		// for (int i = 0; i < 3; i++)
-		// {
-		// 	cin >> arry2[i];
-		// }
-		// cout<< endl;
-
-		distances dis(v1, v2);
-		dis.euclidean(difference_calculator(v1,v2));
-		manhattan_distance(difference_calculator(arry1, arry2));
-		chebyshev(difference_calculator(arry1, arry2));
-		canberra(difference_calculator(arry1, arry2), arry1, arry2);
-		minkowski(difference_calculator(arry1, arry2));
-	}
+	return Dif_V;
+	// dif_arr[0] = abs(v1[0] - v2[0]);
+	// dif_arr[1] = abs(v1[1] - v2[1]);
+	// dif_arr[2] = abs(v1[2] - v2[2]);
+	// return dif_arr;
 }
+
+double Distances:: Manhattan_Distance(vector<double> Dif_V)
+{
+	double sum = 0;
+	for (int i=0; i<Dif_V.size(); i++)
+	{
+		sum += Dif_V[i];
+	}
+	// double distance = dif_v[0] + dif_v[1] + dif_v[2];
+	return sum;
+}
+
+double Distances::euclidean(vector<double>& Dif_V)
+{
+	double dist = 0;
+	for (int i=0; i<Dif_V.size(); i++)
+	{
+		dist += pow(Dif_V[i],2);
+	}
+	// double dist = pow(dif_v[0], 2) + pow(dif_v[1], 2) + pow(dif_v[2], 2);       
+	dist = sqrt(dist);
+	// cout << setprecision(10) << dist;
+	return dist;
+}
+
+
+double Distances::chebyshev(vector<double> Dif_V)
+{
+	double max = 0;
+	for (int i=0; i<Dif_V.size(); i++){
+		if (max<Dif_V[i])
+		{
+			max = Dif_V[i];
+		}
+	}
+	// double max_temp = max(dif_arr[0], dif_arr[1]);
+	// max_temp = max(max_temp, dif_arr[2]);
+	return max;
+}
+
+double Distances:: canberra(vector<double> Dif_V, vector<double> v1, vector<double> v2)
+{
+	double sum = 0;
+	for (int i=0; i< Dif_V.size(); i++)
+	{
+		sum += Dif_V[i]/(v1[i]+v2[i]);
+	}
+	// double sol1 = dif_arr[0] / (arr1[0] + arr2[0]);
+	// double sol2 = dif_arr[1] / (arr1[1] + arr2[1]);
+	// double sol3 = dif_arr[2] / (arr1[2] + arr2[2]);
+	// double dist = sol1 + sol2 + sol3;
+	return sum;
+}
+
+double Distances::minkowski(vector<double>& Dif_V)
+{
+	int sum = 0;
+	for (int i = 0; i < Dif_V.size(); i++)
+	{
+		sum += pow(Dif_V[i], 2);
+	}
+	return sqrt(sum);
+}
+
+int main()
+{
+	vector<double> v1{1, 2, 3};
+	vector<double> v2{3, 2, 1};
+	
+	// double arry1[3];
+	// for (int i = 0; i < 3; i++)
+	// {
+	// 	cin >> arry1[i];
+	// }
+	// cout<< endl;
+	// double arry2[3];
+
+	// for (int i = 0; i < 3; i++)
+	// {
+	// 	cin >> arry2[i];
+	// }
+	// cout<< endl;
+
+	Distances dis(v1, v2);
+	vector<double> difference = dis.Distances::Difference_Calculator(v1,v2);
+	double euc =  dis.Distances::euclidean(difference);
+	double manh = dis.Distances::Manhattan_Distance(difference);
+	double cheb = dis.Distances::chebyshev(difference);
+	double canb = dis.Distances::canberra(difference, v1, v2);
+	double mink = dis.Distances::minkowski(difference);
+	}
+
+	//setprecision(10) לזכור לשים
--- project/distances.h
+++ project/distances.h
@@ -1,11 +1,22 @@
-class distances
+#pragma once
+#define HEADER_distances
+#include<iostream>
+#include<math.h>
+#include<vector>
+#include <iomanip>
+#include<cmath>
+
+using namespace std;
+using std::vector;
+class Distances
 {
    public:
-   
-       void distances::manhattan_distance(double dif_arr[]);
-       double * distances::difference_calculator(double arr1[], double arr2[]);
-       void distances::euclidean(double dif_arr[]);
-       void distances::chebyshev(double dif_arr[]);
-       void distances::canberra(double dif_arr[], double arr1[], double arr2[]);
-       void distances::minkowski(double dif_arr[]);
+       Distances(vector<double> v1, vector<double> v2);
+       Distances();
+       double Manhattan_Distance(vector<double> Dif_V);
+       vector<double> Difference_Calculator(vector<double>& v1, vector<double>& v2);
+       double euclidean(vector<double>& Dif_V);
+       double chebyshev(vector<double> Dif_V);
+       double canberra(vector<double> Dif_V, vector<double> v1, vector<double> v2);
+       double minkowski(vector<double>& Dif_V);
 };
